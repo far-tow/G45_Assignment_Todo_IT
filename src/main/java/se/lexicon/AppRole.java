aed66c3119f8fd64e505f2f55270f760813a7e23
Farhad Towfighian <fartow@gmail.com>
2022-11-18 11:00:38 +0100

    Enum show now regular string text.

--- src/main/java/se/lexicon/AppRole.java
+++ src/main/java/se/lexicon/AppRole.java
@@ -6,5 +6,14 @@
 package se.lexicon;
 
 public enum AppRole {
-    ROLE_APP_USER, ROLE_APP_ADMIN
+    ROLE_APP_USER {
+        public String toString() {
+            return "*USER*";
+        }
+    },
+    ROLE_APP_ADMIN {
+        public String toString() {
+            return "*ADMINISTRATOR*";
+        }
+    };
 }
